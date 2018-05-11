3c7763c2bc082216920b60f7aa5f540c63adf8c4
acabezaslopez <acabezaslopez@victoria11.danielcastelao.org>
2018-05-11 12:49:39 +0200

    Calculadora simple

---
+++ src/boletin27/Boletin27.java
@@ -0,0 +1,17 @@
+package boletin27;
+
+/**
+ *
+ * @author acabezaslopez
+ */
+public class Boletin27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora cal = new Calculadora();
+        cal.setVisible(true);
+    }
+    
+}
